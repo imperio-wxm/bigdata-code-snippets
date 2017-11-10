b17aa1472d14e235f9767b75026df0ecf1233895
wxmimperio <wxmimperio@outlook.com>
2017-11-10 15:41:44 +0800

    change Resources to config.properties

--- hadoop-orc/src/main/java/com/wxmimperio/hadoop/OrcFileMain.java
+++ hadoop-orc/src/main/java/com/wxmimperio/hadoop/OrcFileMain.java
@@ -1,21 +1,11 @@
 package com.wxmimperio.hadoop;
 
-import com.wxmimperio.hadoop.pojo.HiveColumnTypeDesc;
 import com.wxmimperio.hadoop.utils.OrcUtils;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.util.List;
 
 public class OrcFileMain {
 
     public static void main(String[] args) throws Exception {
-        Class.forName("org.apache.hive.jdbc.HiveDriver");
-        String url = "jdbc:hive2://10.1.8.209:10000/dw";
-        String username = "hadoop";
-        try (Connection connection = DriverManager.getConnection(url, username, "")) {
-            List<HiveColumnTypeDesc> hiveColumnTypeDescs = OrcUtils.getPartitionColumns("dw", "cd_item_glog", connection);
-            System.out.println(hiveColumnTypeDescs);
-        }
+        System.out.println(OrcUtils.getColumnTypeDescs("dw", "cd_item_glog"));
     }
 }
--- hadoop-orc/src/main/java/com/wxmimperio/hadoop/utils/OrcUtils.java
+++ hadoop-orc/src/main/java/com/wxmimperio/hadoop/utils/OrcUtils.java
@@ -2,17 +2,36 @@ package com.wxmimperio.hadoop.utils;
 
 import com.wxmimperio.hadoop.pojo.HiveColumnTypeDesc;
 import org.apache.orc.TypeDescription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class OrcUtils {
+    private static final Logger LOG = LoggerFactory.getLogger(OrcUtils.class);
+
+    private static String HIVE_DRIVER = "org.apache.hive.jdbc.HiveDriver";
     private static String url;
     private static String username;
+
+    static {
+        try {
+            Class.forName(HIVE_DRIVER);
+            Properties properties = new Properties();
+            properties.load(OrcUtils.class.getClassLoader().getResourceAsStream("config.properties"));
+            username = properties.getProperty("hive.jdbc.connection.username");
+            url = properties.getProperty("hive.jdbc.connection.url");
+        } catch (ClassNotFoundException e) {
+            LOG.error("HiveDriver not found!", e);
+        } catch (IOException e) {
+            LOG.error("Get config error!", e);
+        }
+    }
 
     /**
      * Jdbc for hive table and get partition key list
@@ -61,12 +80,20 @@ public class OrcUtils {
         TypeDescription schema = TypeDescription.createStruct();
         // jdbc to hive for table info
         try (Connection connection = DriverManager.getConnection(url, username, "")) {
+            List<HiveColumnTypeDesc> ctdList = getPartitionColumns(db, tableName, connection);
+            Set<String> partCol = new HashSet<>();
+            for (HiveColumnTypeDesc ctd : ctdList) {
+                partCol.add(ctd.columnName.toLowerCase());
+            }
             ResultSet res = connection.getMetaData().getColumns(db, db, tableName, "%");
             String columnName, typeName;
             while (res.next()) {
                 columnName = res.getString("COLUMN_NAME");
                 typeName = res.getString("TYPE_NAME");
-                schema.addField(columnName, TypeDescription.fromString(typeName));
+                // partition key not int schema
+                if (!partCol.contains(columnName.toLowerCase())) {
+                    schema.addField(columnName, TypeDescription.fromString(typeName));
+                }
             }
         }
         return schema;
