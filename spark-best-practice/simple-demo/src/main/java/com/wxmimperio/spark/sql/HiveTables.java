3b6b9995b2daea41441ef2c9d8f33c73211fcab4
wxmimperio <wxmimperio@outlook.com>
2019-02-02 17:32:12 +0800

    add jdbc table

---
+++ spark-best-practice/simple-demo/src/main/java/com/wxmimperio/spark/sql/HiveTables.java
@@ -0,0 +1,35 @@
+package com.wxmimperio.spark.sql;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.SparkSession;
+
+import java.io.File;
+
+public class HiveTables {
+
+    public static void main(String[] args) {
+
+        String dwPath = args[0];
+        String dataPath = args[1];
+
+        // Configuration of Hive is done by placing your hive-site.xml, core-site.xml (for security configuration),
+        // and hdfs-site.xml (for HDFS configuration) file in conf/.
+        SparkConf sparkConf = new SparkConf();
+
+        // 创建一个本地的hive metaStore
+        // warehouseLocation points to the default location for managed databases and tables
+        String warehouseLocation = new File(dwPath).getAbsolutePath();
+        sparkConf.set("spark.sql.warehouse.dir", warehouseLocation);
+
+        SparkSession spark = SparkSession.builder()
+                .config(sparkConf)
+                .appName("Java Spark SQL basic example")
+                .master("local")
+                .enableHiveSupport()
+                .getOrCreate();
+
+        spark.sql("CREATE TABLE IF NOT EXISTS person (key INT, value STRING) ROW FORMAT DELIMITED '\t' USING hive OPTIONS(fileFormat 'textfile')");
+        spark.sql("LOAD DATA LOCAL INPATH '" + dataPath + "' OVERWRITE INTO TABLE person PARTITION (part_date='2019-02-01')");
+        spark.sql("SELECT * FROM person").show();
+    }
+}
---
+++ spark-best-practice/simple-demo/src/main/java/com/wxmimperio/spark/sql/JdbcTables.java
@@ -0,0 +1,105 @@
+package com.wxmimperio.spark.sql;
+
+import org.apache.hadoop.yarn.webapp.hamlet.Hamlet;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class JdbcTables {
+
+    public static void main(String[] args) {
+        SparkSession spark = SparkSession.builder()
+                .appName("Java Spark SQL basic example")
+                .master("local")
+                .getOrCreate();
+
+        String mysqlUrl = "jdbc:mysql://127.0.0.1:3306/wxm?useUnicode=true&characterEncoding=utf-8";
+        Properties connectionProperties = new Properties();
+        connectionProperties.put("user", "root");
+        connectionProperties.put("password", "password");
+
+        Dataset<Row> dataSet = spark.read().jdbc(mysqlUrl, "wxm.user", connectionProperties);
+        dataSet.limit(10).show();
+
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            users.add(new User( "wxm_" + i, i, "男"));
+        }
+
+        // 在SaveMode.Overwrite模式下，先truncate表所有数据，再将新数据插入
+        // connectionProperties.put("truncate", "true");
+        Dataset<Row> saveUsers = spark.createDataFrame(users, User.class);
+        saveUsers.write().mode(SaveMode.Append).jdbc(mysqlUrl, "wxm.user", connectionProperties);
+    }
+
+    public static class User {
+        private Integer id;
+        private String name;
+        private Integer age;
+        private String gender;
+
+        public User() {
+        }
+
+
+        public User(String name, Integer age, String gender) {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+        }
+
+        public User(Integer id, String name, Integer age, String gender) {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        public String getGender() {
+            return gender;
+        }
+
+        public void setGender(String gender) {
+            this.gender = gender;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", age=" + age +
+                    ", gender='" + gender + '\'' +
+                    '}';
+        }
+    }
+}
