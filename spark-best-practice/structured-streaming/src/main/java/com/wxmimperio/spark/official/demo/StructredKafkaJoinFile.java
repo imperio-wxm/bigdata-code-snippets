ab058efa7435681887ed07d779274c9c08019048
wxmimperio <wxmimperio@outlook.com>
2019-06-25 10:41:57 +0800

    add kafka join file

---
+++ spark-best-practice/structured-streaming/src/main/java/com/wxmimperio/spark/official/demo/StructredKafkaJoinFile.java
@@ -0,0 +1,62 @@
+package com.wxmimperio.spark.official.demo;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.streaming.OutputMode;
+import org.apache.spark.sql.streaming.Trigger;
+import org.apache.spark.storage.StorageLevel;
+import scala.Tuple2;
+
+import java.util.concurrent.TimeUnit;
+
+public class StructredKafkaJoinFile {
+
+    public static void main(String[] args) throws Exception {
+        SparkConf conf = new SparkConf();
+        conf.setAppName("StructredKafkaJoinFile");
+        conf.setMaster("local");
+
+        // msg {"name":2,"event_time":"2019-06-20 19:09:48"}
+        SparkSession spark = SparkSession.builder().config(conf).getOrCreate();
+        Dataset<Row> df = spark
+                .readStream()
+                .format("kafka")
+                .option("kafka.bootstrap.servers", "10.1.8.132:9092")
+                .option("subscribe", "wxm_test")
+                .option("group.id", "structured-streaming")
+                .option("startingOffsets", "latest")
+                .option("enable.auto.commit", true)
+                .load();
+
+        Dataset<Row> words = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+                .map((MapFunction<Row, Tuple2<String, String>>) row -> {
+                    JsonObject jsonObject = new JsonParser().parse(row.getString(1)).getAsJsonObject();
+                    String eventTime = jsonObject.get("event_time").getAsString();
+                    String name = jsonObject.get("name").getAsString();
+                    return new Tuple2<>(eventTime, name);
+                }, Encoders.tuple(Encoders.STRING(), Encoders.STRING())).selectExpr("_1 as event_time", "_2 as name");
+
+        words.createOrReplaceTempView("wxm_test");
+
+        Dataset<Row> staticDf = spark.read().json("structured-streaming/src/main/resources/people.json");
+        staticDf.createOrReplaceTempView("people");
+        staticDf.persist(StorageLevel.MEMORY_AND_DISK_SER_2());
+        staticDf.show();
+
+        // 聚合操作会触发 shuffle
+        Dataset<Row> streamingSql = spark.sql("select count(*),b.age from wxm_test as a left join people b on a.name = b.name group by b.age");
+        //Dataset<Row> streamingSql = spark.sql("select * from wxm_test");
+        streamingSql.writeStream()
+                .outputMode(OutputMode.Update())
+                .format("console")
+                .trigger(Trigger.ProcessingTime(5, TimeUnit.SECONDS))
+                .start()
+                .awaitTermination();
+    }
+}
