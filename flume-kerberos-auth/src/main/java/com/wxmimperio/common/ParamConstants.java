507aa04531825cd5970816b13968d10c7968deed
wxmimperio <wxmimperio@outlook.com>
2020-07-17 16:26:23 +0800

    update: 添加常量

---
+++ flume-kerberos-auth/src/main/java/com/wxmimperio/common/ParamConstants.java
@@ -0,0 +1,10 @@
+package com.wxmimperio.common;
+
+public class ParamConstants {
+    public static String KERB_CONFIG_PRINCIPAL = "";
+    public static String KERB_KEY_TAB = "";
+    public static String CORE_SITE = "";
+    public static String HDFS_SITE = "";
+    public static String KRB5_CONF = "";
+    public static String PROXY_USER = null;
+}
